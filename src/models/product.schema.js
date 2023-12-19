085b1aab3bbf0782b75dc55e2411a8f60889f9a0
Souvik Nath <130132231+souvikn18@users.noreply.github.com>
2023-12-19 07:37:54 +0000

    product schema added

---
+++ src/models/product.schema.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        trim: true,
+        maxLength: [100, "Product name must not exceed 100 chars"],
+    },
+    price: {
+        type: Number,
+        required: [true, "Price is required"],
+        maxLength: [5, "5 chars price max"],
+    },
+    description: {
+        type: String,
+    },
+    photo: [
+        {
+            secure_url: {
+                type: String,
+                required: true,
+            }
+        }
+    ],
+    stock: {
+        type: Number,
+        default: 0,
+    },
+    sold: {
+        type: Number,
+        default: 0,
+    },
+    collectionId: {
+        ref: "Collection"
+    }
+},{ timestamps: true })
+
+export default mongoose.model("Product", productSchema)
