4b6e55ae13418cadccd3d02262f072b0a3426178
Souvik Nath <130132231+souvikn18@users.noreply.github.com>
2023-12-22 14:52:34 +0000

    s3 aws file upload done

--- src/config/index.js
+++ src/config/index.js
@@ -7,6 +7,11 @@ const config = {
     MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost:27017/ecomm",
     JWT_SECRET: process.env.JWT_SECRET || "yoursecret",
     JWT_EXPIRY: process.env.JWT_EXPIRY || "30d",
+
+    S3_ACCESS_KEY: "KEY VALUE",
+    S3_SECRET_ACCESS_KEY: "SECRET KEY VALUE",
+    S3_BUCKET_NAME: "BUCKET NAME",
+    S3_REGION: "REGION NAME"
 }
 
 export default config;
