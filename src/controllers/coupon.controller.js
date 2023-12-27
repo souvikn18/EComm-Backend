904b5023bf63f1b262bfa8f2e53bdbb2d0c6dac5
Souvik Nath <130132231+souvikn18@users.noreply.github.com>
2023-12-27 12:08:44 +0000

    coupon controller added

--- src/controllers/coupon.controller.js
+++ src/controllers/coupon.controller.js
@@ -25,3 +25,55 @@ export const AddCoupon = asyncHandler(async (req, res) => {
     })
 
 })
+
+export const UpdateCoupon = asyncHandler(async (req, res) => {
+    const {code, discount} = req.body;
+    const {id: coupunId} = req.params;
+
+    if (!code) {
+        throw new CustomError("Need a code to update", 500)
+    }
+    if (!discount) {
+        throw new CustomError("Need a discount amount", 500)
+    }
+
+    const couponToUpdate = await Coupon.findByIdAndUpdate(coupunId, {
+        code,
+        discount
+    }, {
+        new: true,
+        runValidators: true
+    })
+
+    if (!couponToUpdate) {
+        throw new CustomError("Nothing to update", 500)
+    }
+
+    res.status(200).json({
+        success: true,
+        message: "Coupon updated successfully!",
+        couponToUpdate
+    })
+})
+
+export const DisableCoupon = asyncHandler(async(req, res) => {
+    const {active} = req.body;
+
+    if (!active) {
+        throw new CustomError("Coupon is already disabled", 500)
+    }
+
+    const couponToDisable = await Coupon.findOne({active})
+
+    if (!couponToDisable) {
+        throw new CustomError("No coupon found to disable", 404)
+    }
+
+    const disabledCoupon = couponToDisable.set(false)
+
+    res.status(200).json({
+        success: true, 
+        message: "Coupon diabled",
+        disabledCoupon
+    })
+})
