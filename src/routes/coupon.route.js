65687eb02afe1b55c58e8b8db1b1d6587d77e687
Souvik Nath <130132231+souvikn18@users.noreply.github.com>
2024-01-06 15:35:29 +0000

    coupon route added

---
+++ src/routes/coupon.route.js
@@ -0,0 +1,11 @@
+import {Router} from "express"
+import { AddCoupon, deleteCoupon, UpdateCoupon, getAllCoupon, DisableCoupon } from "../controllers/coupon.controller.js"
+import {isLoggedIn, authorize} from "../middleware/auth.middleware.js"
+import AuthRoles from "../utils/authRoles.js"
+
+const router = Router();
+
+router.post('/', isLoggedIn, authorize(AuthRoles.ADMIN), AddCoupon)
+router.post('/:id', isLoggedIn, authorize(AuthRoles.ADMIN || AuthRoles.MODERATOR), deleteCoupon )
+router.put('/action/:id', isLoggedIn, authorize(AuthRoles.ADMIN || AuthRoles.MODERATOR), UpdateCoupon)
+router.get('/', isLoggedIn, getAllCoupon)
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import {Router} from "express"
 import authRoutes from "./auth.routes.js"
+import couponRoutes from './coupon.route.js'
 
 const router = Router;
 
 router.use("/auth", authRoutes)
+router.use("/coupon", couponRoutes)
 
 export default router;
