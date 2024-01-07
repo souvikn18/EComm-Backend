daa16a1b467db0ce8e29ca10942ef67c13f3a34f
Souvik Nath <130132231+souvikn18@users.noreply.github.com>
2024-01-07 06:08:20 +0000

    authRoutes update

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import {SignUp, Login, Logout, getProfile} from "../controllers/auth.controller.js"
+import {SignUp, Login, Logout, getProfile, forgotPassword, resetPassword} from "../controllers/auth.controller.js"
 import {isLoggedIn} from "../middleware/auth.middleware.js"
 
 
@@ -8,6 +8,8 @@ const router = Router();
 router.post("/signup", SignUp)
 router.post("/login", Login)
 router.get("/logout", Logout)
+router.post("/password/forgot", forgotPassword)
+router.post("/password/reset/:token", resetPassword)
 
 router.get("/profile", isLoggedIn , getProfile)
 
